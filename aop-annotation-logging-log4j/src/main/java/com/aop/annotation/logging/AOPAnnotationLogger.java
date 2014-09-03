4cbec7ebf155067b297d582dc60098219be69b27
Ashok Goli <agoli@deloitte.com>
2014-09-04 00:27:14 +0530

    Removed imports

--- aop-annotation-logging-log4j/src/main/java/com/aop/annotation/logging/AOPAnnotationLogger.java
+++ aop-annotation-logging-log4j/src/main/java/com/aop/annotation/logging/AOPAnnotationLogger.java
@@ -10,7 +10,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.stereotype.Component;
 
 /**
  * @author Ashok Goli (agoli)
